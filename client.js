ae7eb9356a2831f9d36f47f9f6e63ec23960b3a4
Kennie <Kenneth Chan>
2022-11-23 15:45:23 -0500

    personalize your snek!

--- client.js
+++ client.js
@@ -9,6 +9,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log('A connection has been made!');
+    conn.write("Name: KNI");
+  });
+
   conn.on('data', (data) => {
     console.log(data)
   });
