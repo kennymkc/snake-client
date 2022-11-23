2804f7c267492d35d064dc8a2dce542d883ca9a2
Kennie <Kenneth Chan>
2022-11-23 17:56:49 -0500

    working snake game

--- play.js
+++ play.js
@@ -4,7 +4,6 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
+const conn = connect();
 
-
-connect();
-setupInput();
+setupInput(conn);
